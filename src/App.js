3feec464797b8478722f2dd2e7679bef215022d9
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-11-14 01:38:04 +0530

    added the addUuser component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import AddUser from "./components/Users/AddUser";
 
 function App() {
-  return <div>dsfxgfcx</div>;
+  return (
+    <React.StrictMode>
+      <AddUser />
+    </React.StrictMode>
+  );
 }
 
 export default App;
